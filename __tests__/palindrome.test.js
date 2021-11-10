8e0aa8b0dd9ea04f7e3c9d3124298976825ec0ae
Frank-Proulx <frankproulx7@gmail.com>
2021-11-09 16:05:11 -0800

    implement generatePalindromes, isSumOfSquares and sumPalindromes having passed tests
    Co-authored-by: Hannah Young <hannah@corgibyte.com>

--- __tests__/palindrome.test.js
+++ __tests__/palindrome.test.js
@@ -1,4 +1,5 @@
 import { Palindrome } from './../src/palindrome.js';
+import { isSumOfSquares } from './../src/palindrome.js';
 
 describe('Palindrome', () => {
 
@@ -7,23 +8,23 @@ describe('Palindrome', () => {
     expect(testPal.num).toEqual(4);
   });
   
-  test('should return false if not palindrome', () => {
-    let testPal = new Palindrome(47);
-    expect(testPal.isPalindrome()).toEqual(false);
-  });
-
-  test('should return true if is palindrome', () => {
-    let testPal = new Palindrome(474);
-    expect(testPal.isPalindrome()).toEqual(true);
+  test('should generate palindromes not palindrome', () => {
+    let testPal = new Palindrome(101);
+    testPal.generatePalindromes();
+    expect(testPal.palindromes).toEqual([1,2,3,4,5,6,7,8,9,11,22,33,44,55,66,77,88,99,101]);
   });
 
   test('should return true if is sum of squares', () => {
-    let testPal = new Palindrome(595);
-    expect(testPal.isPalindrome()).toEqual(true);
+    expect(isSumOfSquares(595)).toEqual(true);
   });
 
   test('should return false if is not sum of squares', () => {
-    let testPal = new Palindrome(594);
-    expect(testPal.isPalindrome()).toEqual(true);
+    expect(isSumOfSquares(594)).toEqual(false);
+  });
+
+  test('should add sum of qualifying palindromes to Palindrome', () => {
+    let testPal = new Palindrome(1000);
+    testPal.sumPalindromes();
+    expect(testPal.sum).toEqual(4164);
   });
 });
--- src/palindrome.js
+++ src/palindrome.js
@@ -2,14 +2,37 @@ export function Palindrome(num) {
   this.num = num;
 }
 
-Palindrome.prototype.isPalindrome = function(num) {
-  
+Palindrome.prototype.generatePalindromes = function() {
+  let array = [];
+  for (let i = 1; i <= this.num; i++) {
+    if (i.toString() === i.toString().split("").reverse().join("")) {
+      array.push(i);
+    }
+  }
+  this.palindromes = array;
 };
 
-Palindrome.prototype.isSumOfSquares = function() {
-
-};
+export function isSumOfSquares(num) {  
+  let foundSum = false;
+  for (let i = 1; i < num && !foundSum; i++) {
+    let currentSum = 0;
+    let currentDigit = i;
+    while (currentSum < num) {
+      currentSum += currentDigit * currentDigit;
+      currentDigit++;
+    }
+    foundSum = currentSum === num && (currentDigit - 1) !== i;
+  }
+  return foundSum;
+}
 
 Palindrome.prototype.sumPalindromes = function() {
-
+  this.generatePalindromes();
+  let sum = 0;
+  this.palindromes.forEach(function(element) {
+    if (isSumOfSquares(element)) {
+      sum += element;
+    }
+  });
+  this.sum = sum;
 };
